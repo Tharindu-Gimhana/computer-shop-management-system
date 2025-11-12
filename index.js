5ffdd68eca5ddd5f2d09512579af7c447981e622
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-11-13 00:15:21 +0530

    add authentication and authorization using json web token

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
 import userrouter from "./routes/userrouter.js";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
 
 
 const app = express();
 app.use(express.json());  //middleware to parse json data
+
+app.use((req,res,next)=>{
+
+    const authorizationheader = req.header("Authorization")
+    console.log(authorizationheader)
+
+    if (authorizationheader != null){
+        const token = authorizationheader.replace("Bearer ","")
+        
+        jwt.verify(token,"SECRETKEY",(error,content)=>{
+        if(content ==null){
+            console.log("invalid user")
+            res.json({message :"invalid token"})
+        }
+        else{
+            console.log(content)
+            req.user=content
+            next( )
+        }
+            
+        }
+        )
+    }else{
+    next()  }
+})
 
  //this is not a middelware , from this part we can turn the traffic come as students to the student route.
 app.use("/users",userrouter )
